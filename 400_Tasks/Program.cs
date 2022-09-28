3d4993b8b2c2d68dac75081d8dd0d780b937759b
PuureVanillasFan <AKleesBomb@yandex.ru>
2022-09-28 14:03:34 +0300

    ya dyelal zadachyu

--- 400_Tasks/Program.cs
+++ 400_Tasks/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more informati
-int a =
-Convert.ToInt32 (Console.ReadLine());
-Console.Write(a*2);
-
-int b =
-Convert.ToInt32 (Console.ReadLine());
-Console.Write(b*2);
+using System;
+namespace Taskassssssssssssssss {
+ class Program {
+   static void Main(string[] args) {
+    Console.Write("Введи целое число ");
+    double x = Convert.ToDouble(Console.ReadLine());
+    if (x>=-5 && x<=-3 || x>=-1 && x<=1 || x>=3 && x<=5) Console.Write("Входит в диапазон");
+    else Console.Write("Не входит в диапазон");
+    Console.ReadKey(); 
+  } 
+ }
+}
