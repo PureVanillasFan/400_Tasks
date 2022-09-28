80480f041735f175413cd7335cade5f6e8464dec
PuureVanillasFan <AKleesBomb@yandex.ru>
2022-09-28 13:18:31 +0300

    first commit

---
+++ 400_Tasks/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more informati
+int a =
+Convert.ToInt32 (Console.ReadLine());
+Console.Write(a*2);
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace ZADACHA {
+  class Program {
+    static void Main(string[] args){
+      Console.WriteLine("напиши 2 разных числа");
+    double n1 = Convert.ToDouble(Console.ReadLine()!);
+     double n2 = Convert.ToDouble(Console.ReadLine());
+     if(n1>=n2) Console.WriteLine("максимальное число =" + n1);
+     else  Console.WriteLine("максимальное число = " + n2);
+    Console.ReadKey();
+    }}}
+     
+      
+     
+     
