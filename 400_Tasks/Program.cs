c3397a3de97c575902f10af169e3b96b16c79510
PuureVanillasFan <AKleesBomb@yandex.ru>
2022-10-04 13:39:50 +0300

    priv

--- 400_Tasks/Program.cs
+++ 400_Tasks/Program.cs
@@ -3,14 +3,20 @@ using System;
 namespace Taskassssssssssssssss {
  class Program {
    static void Main(string[] args) {
-     Console.Write("Введите год целым числом ");
-    int year = Convert.ToInt32(Console.ReadLine());
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 ==0) {    
-       Console.WriteLine(year + " - год высокосный"); 
-    } else {    
-       Console.WriteLine(year + " - год невысокосный"); 
-    }
-    Console.ReadKey(); 
+      Console.Write("Введите целое шестизначное число:  ");
+    if (! int.TryParse(Console.ReadLine(), out int n)) {
+       Console.WriteLine("вы ввели некорректное число");
+       return;
+    }  
+    int cst = n / 100000;
+    int cdt = n / 10000 % 10;
+    int cet = n / 1000 % 10;
+    int cs  = n / 100 % 10;
+    int cd  = n / 10 % 10;
+    int ce  = n % 10;
+    Console.Write(cst + cdt + cet == cs + cd + ce ? 
+                      "номер билета счастливый" : "номер билета не счастливый");
+    Console.ReadKey();
   }
  }
 }
